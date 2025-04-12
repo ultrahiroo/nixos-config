8a2ef6c7521d40014993785b2579b54a74821f1f
Inoue Hiroo <ultrahiroo2000@yahoo.co.jp>
2025-04-12 20:57:12 +0900

    WIP

--- home/plasma/default.nix
+++ home/plasma/default.nix
@@ -1,6 +1,9 @@
 { ... }: {
-  home.file.".config/kdedefaults/kdeglobals".source = ./kdedefaults/kdeglobals;
-  home.file.".config/kcminputrc".source = ./kcminputrc;
-  home.file.".config/kscreenlockerrc".source = ./kscreenlockerrc;
-  home.file.".config/kxkbrc".source = ./kxkbrc;
+  home.file = {
+    ".config/kdedefaults/kdeglobals".source = ./kdedefaults/kdeglobals;
+    ".config/kcminputrc".source = ./kcminputrc;
+    ".config/kscreenlockerrc".source = ./kscreenlockerrc;
+    ".config/kxkbrc".source = ./kxkbrc;
+    ".config/powerdevilrc".source = ./powerdevilrc;
+  };
 }
