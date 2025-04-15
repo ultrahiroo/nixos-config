b1ab3ab65ec38cd3b1c565b9b4a47dffe3e65a66
Inoue Hiroo <ultrahiroo2000@yahoo.co.jp>
2025-04-15 15:48:58 +0900

    WIP

--- home/yash/export.sh
+++ home/yash/export.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/yash
-
 #USER=root
 #SHLVL=1
 #OLDPWD=/Users/Root
@@ -16,7 +14,7 @@
 #export MANPATH=
 #export INFOPATH=
 
-export HOSTNAME="$(uname -n)"
+# export HOSTNAME="$(uname -n)"
 export LINES='25'
 
 JOB="$(($(nproc) - 1))"
@@ -64,11 +62,11 @@ else
     export LANG='ja_JP.UTF-8'
 fi
 
-INPUT_METHOD='fcitx'
-export GTK_IM_MODULE="${INPUT_METHOD}"
-export QT_IM_MODULE="${INPUT_METHOD}"
-export XMODIFIERS="@im=${INPUT_METHOD}"
-export GLFW_IM_MODULE="${INPUT_METHOD}"
+# INPUT_METHOD='fcitx'
+# export GTK_IM_MODULE="${INPUT_METHOD}"
+# export QT_IM_MODULE="${INPUT_METHOD}"
+# export XMODIFIERS="@im=${INPUT_METHOD}"
+# export GLFW_IM_MODULE="${INPUT_METHOD}"
 
 # export $(dbus-launch)
 
@@ -84,17 +82,17 @@ export GLFW_IM_MODULE="${INPUT_METHOD}"
 export GIT_DISCOVERY_ACROSS_FILESYSTEM='1' # Git on Btrfs
 #export LIBGL_ALWAYS_SOFTWARE=1 # Kitty, Servo
 
-if test "${XDG_SESSION_TYPE}" = 'wayland'; then
-    export MOZ_ENABLE_WAYLAND='1'  # Firefox
-    export QT_QPA_PLATFORM='wayland'
-    # export QT_WAYLAND_DISABLE_WINDOWDECORATION='1' # Sway
-    export _JAVA_AWT_WM_NONREPARENTING='1'  # https://github.com/swaywm/sway/issues/595
-fi
+# if test "${XDG_SESSION_TYPE}" = 'wayland'; then
+#     export MOZ_ENABLE_WAYLAND='1'  # Firefox
+#     export QT_QPA_PLATFORM='wayland'
+#     # export QT_WAYLAND_DISABLE_WINDOWDECORATION='1' # Sway
+#     export _JAVA_AWT_WM_NONREPARENTING='1'  # https://github.com/swaywm/sway/issues/595
+# fi
 
-export BROWSER='chromium-browser'
+# export BROWSER='chromium-browser'
 export EDITOR='nvim'
 # export EDITOR='micro'
-export PAGER='bat'
+# export PAGER='bat'
 
 #export PATH=$HOME/bin:$PATH
 #export PATH=/usr/bin:$PATH
@@ -102,62 +100,62 @@ export PAGER='bat'
 #export PATH=/opt/vc/bin:$PATH
 #export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
 
-export SETTING="/user/${USER}/setting"
-export CACHE="/user/${USER}/cache"
+# export SETTING="/user/${USER}/setting"
+# export CACHE="/user/${USER}/cache"
 
-export XDG_CACHE_HOME="${CACHE}"
-export XDG_CONFIG_HOME="${CACHE}"
-export XDG_DATA_HOME="${CACHE}"
-export XDG_DATA_DIRS='/usr/share'
+# export XDG_CACHE_HOME="${CACHE}"
+# export XDG_CONFIG_HOME="${CACHE}"
+# export XDG_DATA_HOME="${CACHE}"
+# export XDG_DATA_DIRS='/usr/share'
 
-export XDG_DESKTOP_DIR="${HOME}/Desktop"
-export XDG_DOCUMENTS_DIR="${HOME}/Documents"
-export XDG_DOWNLOAD_DIR="${HOME}/Downloads"
-export XDG_MUSIC_DIR="${HOME}/Music"
-export XDG_PICTURES_DIR="${HOME}/Pictures"
-export XDG_PUBLICSHARE_DIR="${HOME}/Public"
-export XDG_TEMPLATES_DIR="${HOME}/.Templates"
-export XDG_VIDEOS_DIR="${HOME}/Videos"
+# export XDG_DESKTOP_DIR="${HOME}/Desktop"
+# export XDG_DOCUMENTS_DIR="${HOME}/Documents"
+# export XDG_DOWNLOAD_DIR="${HOME}/Downloads"
+# export XDG_MUSIC_DIR="${HOME}/Music"
+# export XDG_PICTURES_DIR="${HOME}/Pictures"
+# export XDG_PUBLICSHARE_DIR="${HOME}/Public"
+# export XDG_TEMPLATES_DIR="${HOME}/.Templates"
+# export XDG_VIDEOS_DIR="${HOME}/Videos"
 
-export GNUPGHOME="${CACHE}/gnupg"
-export GOPATH="${CACHE}/go"
-export MPLAYER_HOME="${CACHE}/mplayer"
-export XAUTHORITY="${CACHE}/xorg/Xauthority"
-export RUSTUP_HOME="${CACHE}/rustup"
-export CARGO_HOME="${CACHE}/cargo"
-export SCCACHE_DIR="$CACHE/sccache"
-export SCCACHE_CACHE_SIZE='4G'
-export NPM_CONFIG_PREFIX="${CACHE}/npm"
-export NPM_CONFIG_USERCONFIG="${CACHE}/npm/npmrc"
-#export DENO_INSTALL=$HOME/.deno
-#export PATH=$DENO_INSTALL/bin:$PATH
-export FOSSIL_HOME="${CACHE}/fossil"
-export FOSSIL_USER='one'
-export PERL5LIB="${CACHE}/perl"
-export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD='true'
-export PUPPETEER_EXECUTABLE_PATH="$(command -v chromium)"
-export PYTHONUSERBASE="${CACHE}/python"
-export IPFS_PATH="${CACHE}/ipfs"
-export GITEA_WORK_DIR="${CACHE}/gitea"
-export TESSDATA_PREFIX="${CACHE}/tessdata-legacy"  # Audiveris
-export MODULAR_HOME="${CACHE}/modular"
+# export GNUPGHOME="${CACHE}/gnupg"
+# export GOPATH="${CACHE}/go"
+# export MPLAYER_HOME="${CACHE}/mplayer"
+# export XAUTHORITY="${CACHE}/xorg/Xauthority"
+# export RUSTUP_HOME="${CACHE}/rustup"
+# export CARGO_HOME="${CACHE}/cargo"
+# export SCCACHE_DIR="$CACHE/sccache"
+# export SCCACHE_CACHE_SIZE='4G'
+# export NPM_CONFIG_PREFIX="${CACHE}/npm"
+# export NPM_CONFIG_USERCONFIG="${CACHE}/npm/npmrc"
+# export DENO_INSTALL=$HOME/.deno
+# export PATH=$DENO_INSTALL/bin:$PATH
+# export FOSSIL_HOME="${CACHE}/fossil"
+# export FOSSIL_USER='one'
+# export PERL5LIB="${CACHE}/perl"
+# export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD='true'
+# export PUPPETEER_EXECUTABLE_PATH="$(command -v chromium)"
+# export PYTHONUSERBASE="${CACHE}/python"
+# export IPFS_PATH="${CACHE}/ipfs"
+# export GITEA_WORK_DIR="${CACHE}/gitea"
+# export TESSDATA_PREFIX="${CACHE}/tessdata-legacy"  # Audiveris
+# export MODULAR_HOME="${CACHE}/modular"
 
-if test -z "${SHLVL}" -o "${SHLVL}" = '1'; then
-    export PATH=$CACHE/python/bin:$PATH
-    export PATH=$GOPATH/bin:$PATH
-    export PATH=$NPM_CONFIG_PREFIX/bin:$PATH
-    export PATH=$CARGO_HOME/bin:$PATH
-    export PATH=${CACHE}/bin:$PATH
-    export PATH=/opt/arm/arm-instruction-emulator-21.0_Generic-AArch64_Ubuntu-18.04_aarch64-linux/bin64:$PATH
+# if test -z "${SHLVL}" -o "${SHLVL}" = '1'; then
+#     export PATH=$CACHE/python/bin:$PATH
+#     export PATH=$GOPATH/bin:$PATH
+#     export PATH=$NPM_CONFIG_PREFIX/bin:$PATH
+#     export PATH=$CARGO_HOME/bin:$PATH
+#     export PATH=${CACHE}/bin:$PATH
+#     export PATH=/opt/arm/arm-instruction-emulator-21.0_Generic-AArch64_Ubuntu-18.04_aarch64-linux/bin64:$PATH
     # export PYTHONPATH=$CACHE/python/lib/python3.9/site-packages
     # export PYTHONPATH=$CACHE/apostrophe/lib/python3.9/site-packages:$PYTHONPATH
     # export PYTHONPATH=/user/one/home/miniforge3:$PYTHONPATH
     # export PYTHONPATH=${CACHE}/GIMP/2.10/plug-ins/gimpfu:${PYTHONPATH}
     # export PATH="/user/one/home/miniforge3/bin:${PATH}"
-    export PATH="${MODULAR_HOME}/pkg/packages.modular.com_mojo/bin:${PATH}"
-    # MAX_PATH="$(modular config max.path)"
-    # export PATH="${MAX_PATH}/bin:${PATH}"
-fi
+#     export PATH="${MODULAR_HOME}/pkg/packages.modular.com_mojo/bin:${PATH}"
+#     # MAX_PATH="$(modular config max.path)"
+#     # export PATH="${MAX_PATH}/bin:${PATH}"
+# fi
 
 #export XKB_DEFAULT_RULES=''
 #export XKB_DEFAULT_MODEL=''
@@ -170,4 +168,4 @@ fi
 
 # export MYPY_CACHE_DIR='/tmp/mypy'
 
-export NIXPKGS_ALLOW_UNFREE=1
+# export NIXPKGS_ALLOW_UNFREE=1
