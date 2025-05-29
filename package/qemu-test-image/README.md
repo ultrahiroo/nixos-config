e580b495811566136c6d273e7bfc11708ec59e5b
Inoue Hiroo <ultrahiroo2000@yahoo.co.jp>
2025-05-29 12:33:36 +0900

    WIP

---
+++ package/qemu-test-image/README.md
@@ -0,0 +1,7 @@
+# Command
+```sh
+nix build .
+```
+
+# Reference
+- https://github.com/nix-community/disko/blob/master/docs/disko-images.md
---
+++ package/qemu-test-image/flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+        inherit system;
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs, system }: {
+        default = let
+          in
+            pkgs.writeShellApplication {
+              name = "qemu-test-image";
+              runtimeInputs = with pkgs; [
+                qemu
+              ];
+              text = ''
+                if [ -z "$1" ]; then
+                  echo "Usage: $0 <path-to-boot-image>"
+                  exit 1
+                fi
+                tmpFile=$(mktemp /tmp/test-image.XXXXXX)
+                trap 'rm -f $tmpFile' EXIT
+                cp "$1" "$tmpFile"
+                qemu-system-x86_64 \
+                  -enable-kvm \
+                  -m 2G \
+                  -cpu max \
+                  -smp 2 \
+                  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+                  -device virtio-net-pci,netdev=net0 \
+                  -drive if=pflash,format=raw,readonly=on,file=${pkgs.OVMF.firmware} \
+                  -drive if=pflash,format=raw,readonly=on,file=${pkgs.OVMF.variables} \
+                  -drive if=virtio,format=raw,file="$tmpFile" \
+                  # -nographic
+              '';
+            };
+      });
+    };
+}
