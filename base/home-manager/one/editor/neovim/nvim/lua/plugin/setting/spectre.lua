0d69870972c366fcd84cc4ade50e16a439b2ad19
Inoue Hiroo <ultrahiroo2000@yahoo.co.jp>
2025-09-01 12:06:30 +0900

    WIP

--- base/home-manager/one/editor/neovim/nvim/lua/plugin/setting/spectre.lua
+++ base/home-manager/one/editor/neovim/nvim/lua/plugin/setting/spectre.lua
@@ -3,6 +3,20 @@ return {
     config = function()
         require('spectre').setup({
         })
+        vim.api.nvim_create_user_command(
+            'SpectreReplace',
+            function()
+                require('spectre.actions').run_replace()
+            end,
+            {}
+        )
+        vim.api.nvim_create_user_command(
+            'SpectreDeleteLine',
+            function()
+                require('spectre.actions').run_delete_line()
+            end,
+            {}
+        )
     end,
     cmd = {
         'Spectre',
