a9240204f4a1552e6a9ef4b05e0dc3e68d56c694
Inoue Hiroo <ultrahiroo2000@yahoo.co.jp>
2026-02-09 14:46:17 +0900

    ADD

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,14 @@
 let
   main = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgIfC7+nBWX8ri8DAYaiBEK9eGdp2me3emN/JqcjXcU";
+  rpi-btrfs = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK1kBSlJydDL8tv1tULpDQdB8n98RDvKnPpacLgGDxJ3";
 in
 {
-  "user_one_password.age".publicKeys = [ main ];
-  "forgejo_admin_password.age".publicKeys = [ main ];
+  "user_one_password.age".publicKeys = [
+    main
+    rpi-btrfs
+  ];
+  "forgejo_admin_password.age".publicKeys = [
+    main
+    rpi-btrfs
+  ];
 }
