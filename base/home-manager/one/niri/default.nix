81ef5f264090d528b231c77e2e7b8894db11b193
Inoue Hiroo <ultrahiroo2000@yahoo.co.jp>
2026-02-09 09:56:34 +0900

    FIX

--- base/home-manager/one/niri/default.nix
+++ base/home-manager/one/niri/default.nix
@@ -1,28 +1,22 @@
 { pkgs, ... }:
 {
-  xdg = {
-    configFile = {
-      "niri" = {
-        source = ./niri;
-        force = true;
-      };
-    };
+  xdg.configFile."niri" = {
+    source = ./niri;
+    force = true;
   };
-  home = {
-    packages = with pkgs; [
-      gnome-keyring
-      mako
-      nwg-drawer
-      quickshell
-      waybar
-      wl-clipboard
-      wl-clip-persist
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-gnome
-      xwayland-satellite
-      # (pkgs.writeScriptBin "focus-window" (builtins.readFile ./bin/focus-window.fish))
-      # (pkgs.writeScriptBin "sort-windows" (builtins.readFile ./binsort-windows.fish))
-      # (pkgs.writeScriptBin "wait-window" (builtins.readFile ./bin/wait-window.fish))
-    ];
-  };
+  home.packages = with pkgs; [
+    gnome-keyring
+    mako
+    nwg-drawer
+    quickshell
+    waybar
+    wl-clipboard
+    wl-clip-persist
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-gnome
+    xwayland-satellite
+    # (pkgs.writeScriptBin "focus-window" (builtins.readFile ./bin/focus-window.fish))
+    # (pkgs.writeScriptBin "sort-windows" (builtins.readFile ./binsort-windows.fish))
+    # (pkgs.writeScriptBin "wait-window" (builtins.readFile ./bin/wait-window.fish))
+  ];
 }
